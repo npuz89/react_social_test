d9aa7f562e5eadb2b7d4c5dc2c45a5adbe8c2c93
npuz89 <rkp1989v@gmail.com>
2021-02-24 18:06:21 +0500

    NavLink

--- src/include/leftmenu.js
+++ src/include/leftmenu.js
@@ -1,14 +1,14 @@
 import s from "./css/leftmenu.module.css"
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function Leftmenu() {
     return (
         <div className={s.leftmenu}>
             <ul>
-                <li><Link to="/profile" className={`${s.item} ${s.active}`}>Profile</Link></li>
-                <li><Link to="/dialogs" className={s.item}>Dialogs</Link></li>
-                <li><Link to="/music" className={s.item}>Music</Link></li>
-                <li><Link to="/news" className={s.item}>News</Link></li>
+                <li className={s.item}><NavLink to="/profile" activeClassName={s.active}>Profile</NavLink></li>
+                <li className={s.item}><NavLink to="/dialogs" activeClassName={s.active}>Dialogs</NavLink></li>
+                <li className={s.item}><NavLink to="/music" activeClassName={s.active}>Music</NavLink></li>
+                <li className={s.item}><NavLink to="/news" activeClassName={s.active}>News</NavLink></li>
                 <li><a href="#" className={s.item}>Settings</a></li>
 
             </ul>
